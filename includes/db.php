a2fc52964f78e29de77decef430eebf6f5a38e55
Francis David <francdavies03@gmail.com>
2026-04-20 09:33:38 +0100

    Modified the db.php file for render deploy

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,7 @@
 <?php
 
-$db['db_host'] = "localhost";
+#Local Database
+/*$db['db_host'] = "localhost";
 $db['db_user'] = "root";
 $db['db_pass'] = "";
 $db['db_name'] = "cms";
@@ -10,6 +11,32 @@ define(strtoupper ($key), $value);
 }
 
 $connection =
-mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);*/
+
+
+#Live web Database
+
+$db['db_host'] = getenv("DB_HOST");
+$db['db_user'] = getenv("DB_USER");
+$db['db_pass'] = getenv("DB_PASS");
+$db['db_name'] = getenv("DB_NAME");
+$db['db_port'] = getenv("DB_PORT");
+
+foreach ($db as $key => $value) {
+    define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(
+    DB_HOST,
+    DB_USER,
+    DB_PASS,
+    DB_NAME,
+    DB_PORT
+);
+
+if (!$connection) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
 
 ?>
